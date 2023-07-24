41699b08900e4cf25f6c19e2d1b8637db9bc668e
yangjaehyuk <gndksakxk10@naver.com>
2023-07-24 19:38:53 +0900

    refactor: Refactor main.css & main.js

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ HeadMenuEls.addEventListener('click', function(e) {
     NaviEl.style.display="block";
     HeadLogoEl.style.display='none';
     SearchEl.style.display='block';
-    // HeadLines1.classList.add("disappear");
     HeadLines1.style.animation='420ms ease 0s 1 normal forwards running disappear';
     HeadLines2.style.animation='240ms ease 180ms 1 normal forwards running leftX';
     HeadLines3.style.animation='240ms ease 180ms 1 normal forwards running rightX';

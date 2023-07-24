1522cd3e54aa94ea1eb1e303d406aa6899b7fb12
89882 <pgh123166@naver.com>
2023-07-24 22:29:36 +0900

    feat: 자동 슬라이드 구현

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 const container = document.querySelector('.items');
 const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
+const wrapper = document.querySelector('.wrapper');
 
+let loopInterval = setInterval(() => {
+  translateContainer(-1); // 다음 슬라이드를 보여주는 함수
+}, 3000);
+wrapper.addEventListener('mouseout', () => {
+  loopInterval = setInterval(() => {
+    translateContainer(-1); // 다음 슬라이드를 보여주는 함수
+  }, 3000);
+});
+wrapper.addEventListener('mouseover', () => {
+  clearInterval(loopInterval);
+});
+//자동으로 슬라이드를 움직이게 하는 함수
+// 이 기능으로 인해 js를 사용
 (function addEvent() {
   prevBtn.addEventListener('click', translateContainer.bind(this, 1));
   nextBtn.addEventListener('click', translateContainer.bind(this, -1));
@@ -11,7 +25,11 @@ function translateContainer(direction) {
   console.log(container.lastElementChild);
   const selectedBtn = direction === 1 ? 'prev' : 'next';
   container.style.transitionDuration = '500ms';
-  container.style.transform = `translateX(-1620px)`;
+  if (direction === -1) {
+    container.style.transform = `translateX(-1620px)`;
+  } else {
+    container.style.transform = `translateX(560px)`;
+  }
   //5초동안 gap + 이미지 크기 + 이미지 절반 크기인 1620px만큼 움직임
   container.ontransitionend = () => reorganizeEl(selectedBtn);
 }

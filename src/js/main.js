7d51793b615f5a030ea6e808789fb492d90c2b9e
skyeome <tjru7230@naver.com>
2023-07-25 13:03:54 +0900

    Feature: Add the concept section

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 // main.js
 import "../styles/index.scss";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Swiper from "swiper";
 import { Navigation, Pagination, EffectFade } from "swiper/modules";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.min.css";
-
+gsap.registerPlugin(ScrollTrigger);
 (function (w, d) {
   // 윈도우의 컨텐츠가 로드 되면 아래 스크립트들을 실행합니다.
   window.addEventListener("DOMContentLoaded", (event) => {
@@ -15,14 +15,15 @@ import "swiper/swiper.min.css";
     const hamberger = document.getElementById("hamberger-btn");
     let isOpen = false;
 
-    // ScrollTrigger.create({
-    //   start: "top 100vh",
-    //   end: 99999,
-    //   toggleClass: {
-    //     className: "scrolled",
-    //     targets: "#header",
-    //   },
-    // });
+    ScrollTrigger.create({
+      trigger: "#kv",
+      start: "top bottom",
+      end: "bottom top",
+      toggleClass: {
+        className: "header--scrolled",
+        targets: "#header",
+      },
+    });
 
     // init Swiper:
     const swiper = new Swiper(".swiper.main-visual--swiper", {
@@ -34,6 +35,19 @@ import "swiper/swiper.min.css";
       navigation: {
         nextEl: ".swiper-controls__next-btn",
         prevEl: ".swiper-controls__prev-btn",
+      },
+    });
+
+    // init Swiper:
+    const conceptSwiper = new Swiper(".swiper.the-concept__img-swiper", {
+      // configure Swiper to use modules
+      modules: [Navigation, EffectFade],
+      loop: true,
+      effect: "fade",
+      // Navigation arrows
+      navigation: {
+        nextEl: ".the-concept__next-btn",
+        prevEl: ".the-concept__prev-btn",
       },
     });
 

73c80f6553bfbb4dd79582eb1e1bd925aaa98bf1
89882 <pgh123166@naver.com>
2023-07-25 16:43:22 +0900

    feat: 슬라이더 information 구현

--- src/main.js
+++ src/main.js
@@ -3,28 +3,33 @@ const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
 const wrapper = document.querySelector('.wrapper');
 
-let loopInterval = setInterval(() => {
-  translateContainer(-1); // 다음 슬라이드를 보여주는 함수
-}, 3000);
-wrapper.addEventListener('mouseout', () => {
-  loopInterval = setInterval(() => {
-    translateContainer(-1); // 다음 슬라이드를 보여주는 함수
-  }, 3000);
-});
-wrapper.addEventListener('mouseover', () => {
-  clearInterval(loopInterval);
-});
+// let loopInterval = setInterval(() => {
+//   translateContainer(-1); // 다음 슬라이드를 보여주는 함수
+// }, 3000);
+// wrapper.addEventListener('mouseout', () => {
+//   loopInterval = setInterval(() => {
+//     translateContainer(-1); // 다음 슬라이드를 보여주는 함수
+//   }, 3000);
+// });
+// wrapper.addEventListener('mouseover', () => {
+//   clearInterval(loopInterval);
+// });
 //자동으로 슬라이드를 움직이게 하는 함수
 // 이 기능으로 인해 js를 사용
 (function addEvent() {
+  let information = container.children[3].firstElementChild;
+  information.style.opacity = 1;
+  information.style.visibility = 'visible';
   prevBtn.addEventListener('click', translateContainer.bind(this, 1));
   nextBtn.addEventListener('click', translateContainer.bind(this, -1));
 })();
 
 function translateContainer(direction) {
-  console.log(container.lastElementChild);
   const selectedBtn = direction === 1 ? 'prev' : 'next';
   container.style.transitionDuration = '500ms';
+  let information = container.children[3].firstElementChild;
+  information.style.visibility = 'hidden';
+  information.style.opacity = 0;
   if (direction === -1) {
     container.style.transform = `translateX(-1620px)`;
   } else {
@@ -36,6 +41,12 @@ function translateContainer(direction) {
 
 function reorganizeEl(selectedBtn) {
   container.removeAttribute('style');
+  let information =
+    selectedBtn === 'prev'
+      ? container.children[2].firstElementChild
+      : container.children[4].firstElementChild;
+  information.style.opacity = 1;
+  information.style.visibility = 'visible';
   selectedBtn === 'prev'
     ? container.insertBefore(
         container.lastElementChild,

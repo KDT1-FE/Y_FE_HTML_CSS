7f27b133750a71c3aec1ad660e13fbc2d16c6706
HOOOO98 <loading2us@gmail.com>
2023-07-26 11:52:02 +0900

    Docs: Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
+// Header language box
 const languageEL = document.querySelector('header .mainmenu__language');
 const language__basketEL = languageEL.querySelector('.mainmenu__language__basket');
 
-languageEL.addEventListener('click', function () {
+languageEL.addEventListener('click', function (event) {
+    event.stopPropagation();
     if (language__basketEL.classList.contains('show')) {
-        language__basketEL.remove('show');
+        language__basketEL.classList.remove('show');
     } else {
-        language__basketEL.add('show');
+        language__basketEL.classList.add('show');
     }
     
+})
+
+window.addEventListener('click', function () {
+    language__basketEL.classList.remove('show')
+})
+
+// Footer family sites box
+const familysiteEL = document.querySelector('footer .attachment__rightitem');
+const familysite__basketEL = familysiteEL.querySelector('.familysite_basket');
+
+familysiteEL.addEventListener('click', function (event) {
+    event.stopPropagation();
+    if (familysite__basketEL.classList.contains('show')) {
+        familysite__basketEL.classList.remove('show')
+    }   else {
+        familysite__basketEL.classList.add('show')
+    }
+})
+
+window.addEventListener('click', function () {
+    familysite__basketEL.classList.remove('show')
 })

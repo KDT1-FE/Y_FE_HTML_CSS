15d443b3006e5d1b07dcec2945cb9d1541deceef
TaePoong719 <just5538@gmail.com>
2023-07-26 15:25:30 +0900

    Feat: home2, home3 애니메이션 추가

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ setTimeout(()=>{
 const windowHeight = window.outerHeight
 
 const homeEventHandler = ()=>{
+    // 추후 removeHandler를 통하여 변수들을 제거함으로서 메모리 반환 ~ 메모리 최적화
+    
     const homeText = document.querySelector('.home_text')
     const homeIphone1 = document.querySelector('.home_image_container .iphone_wrap:first-of-type')
     const homeIphone2 = document.querySelector('.home_image_container .iphone_wrap:last-of-type')
@@ -52,7 +54,6 @@ const homeEventHandler = ()=>{
         },300)
 
         // 성능 최적화를 위하여 한번 사용한 eventListener를 제거 => 딱 한번만 동작하게 됨
-        // 또한 위의 콜백 함수 상단에서 선언한 변수들을 제거함으로서 메모리 사용 최적화
         window.removeEventListener('scroll',homeEventHandler)
     }
 }
@@ -61,4 +62,86 @@ window.addEventListener('scroll',homeEventHandler)
 
 
 /* home2 애니메이션 */
+
+const home2EventHandler = ()=>{
+    const home2Text = document.querySelector('.home2_textwrap')
+    const home2Contents = document.querySelectorAll('.home2_content')
+    const home2Images = document.querySelectorAll('.home2_image_container')
+
+    // window 화면 상단에서 한 요소의 가장 높은 위치까지 크기와 윈도우 높이를 비교
+    // console.log(homeText.getBoundingClientRect().top < windowHeight) 
+    if(home2Text.getBoundingClientRect().top < windowHeight-200){
+        setTimeout(()=>{
+            home2Text.style.animation = 'appear_from_bottom ease 1.5s'
+            home2Text.style.opacity = 1
+            // 화면에 시간간격마다 차례대로 화면에 요소를 띄움, 띄어지는 요소는 CSS animation 이 걸려있어서 부드럽게 동작
+            setTimeout(()=>{
+                home2Contents.forEach(item=>{
+                    item.style.animation = 'appear_from_bottom ease 1.5s'
+                    item.style.opacity = 1
+                })
+                
+                setTimeout(()=>{
+                    home2Images.forEach(item=>{
+                        item.style.animation = 'appear_from_bottom ease 1.5s'
+                        item.style.opacity = 1
+                    })
+                },600)
+            },600)
+        },200)
+
+        // 성능 최적화를 위하여 한번 사용한 eventListener를 제거 => 딱 한번만 동작하게 됨
+        // 또한 위의 콜백 함수 상단에서 선언한 변수들을 제거함으로서 메모리 사용 최적화
+        window.removeEventListener('scroll',home2EventHandler)
+    }
+}
+
+window.addEventListener('scroll',home2EventHandler)
+
+/* home3  */
+
+const home3EventHandler = ()=>{
+    const home3Text = document.querySelector('.home3_textwrap')
+    const home3ImageContainer = document.querySelector('.home3_image_container .iphone_wrap')
+    const home3P = document.querySelector('.home3_textwrap2 p')
+    const home3TextSpans = document.querySelectorAll('.home3_textwrap2 h3')
+
+    // window 화면 상단에서 한 요소의 가장 높은 위치까지 크기와 윈도우 높이를 비교
+    // console.log(homeText.getBoundingClientRect().top < windowHeight) 
+    if(home3Text.getBoundingClientRect().top < windowHeight-200){
+        setTimeout(()=>{
+            home3Text.style.animation = 'appear_from_bottom ease 1.5s'
+            home3Text.style.opacity = 1
+            // 화면에 시간간격마다 차례대로 화면에 요소를 띄움, 띄어지는 요소는 CSS animation 이 걸려있어서 부드럽게 동작
+            setTimeout(()=>{
+                home3ImageContainer.style.animation = 'appear_from_bottom ease 1.5s'
+                home3ImageContainer.style.opacity = 1
+                
+                setTimeout(()=>{
+                    home3P.style.animation = 'appear_from_bottom ease 1.5s'
+                    home3P.style.opacity = 1
+
+                    console.log(home3TextSpans)
+                    setTimeout(()=>{
+                        home3TextSpans[0].style.animation = 'appear_from_bottom ease 1.5s'
+                        home3TextSpans[0].style.opacity = 1
+                        setTimeout(()=>{
+                            home3TextSpans[1].style.animation = 'appear_from_bottom ease 1.5s'
+                            home3TextSpans[1].style.opacity = 1
+                            setTimeout(()=>{
+                                home3TextSpans[2].style.animation = 'appear_from_bottom ease 1.5s'
+                                home3TextSpans[2].style.opacity = 1
+                            },600)
+                        },600)
+                    },600)                   
+                },600)
+            },600)
+        },300)
+        // 성능 최적화를 위하여 한번 사용한 eventListener를 제거 => 딱 한번만 동작하게 됨
+        // 또한 위의 콜백 함수 상단에서 선언한 변수들을 제거함으로서 메모리 사용 최적화
+        window.removeEventListener('scroll',home3EventHandler)
+    }
+}
+window.addEventListener('scroll',home3EventHandler)
+
 

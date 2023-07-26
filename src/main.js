a44af67206b1263cbbcd35fb3f9d1d3c1d7d355f
89882 <pgh123166@naver.com>
2023-07-26 11:20:18 +0900

    feat: sub-slider 구현

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ const container = document.querySelector('.items');
 const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
 const wrapper = document.querySelector('.wrapper');
-
+const mainWrapper = document.querySelector('.main-wrapper');
+const mainItems = document.querySelector('.main-items');
+const mainPrev = document.querySelector('.main-prev');
+const mainNext = document.querySelector('.main-next');
+const resignItems = document.querySelector('.sub-items');
+const resignPrev = document.querySelector('.sub-prev');
+const resignNext = document.querySelector('.sub-next');
 // let loopInterval = setInterval(() => {
 //   translateContainer(-1); // 다음 슬라이드를 보여주는 함수
 // }, 3000);
@@ -16,13 +22,58 @@ const wrapper = document.querySelector('.wrapper');
 // });
 //자동으로 슬라이드를 움직이게 하는 함수
 // 이 기능으로 인해 js를 사용
-(function addEvent() {
-  let information = container.children[3].firstElementChild;
-  information.style.opacity = 1;
-  information.style.visibility = 'visible';
-  prevBtn.addEventListener('click', translateContainer.bind(this, 1));
-  nextBtn.addEventListener('click', translateContainer.bind(this, -1));
-})();
+
+function translateMainWrapper(direction) {
+  const selectedBtn = direction === 1 ? 'prev' : 'next';
+  mainItems.style.transitionDuration = '300ms';
+  if (direction === -1) {
+    mainItems.style.transform = `translateX(-1060px)`;
+  } else {
+    mainItems.style.transform = `translateX(1060px)`;
+  }
+  mainItems.ontransitionend = () => reorganizeMainItems(selectedBtn);
+}
+
+function reorganizeMainItems(selectedBtn) {
+  mainItems.removeAttribute('style');
+  selectedBtn === 'prev'
+    ? mainItems.insertBefore(
+        mainItems.lastElementChild,
+        mainItems.firstElementChild
+      )
+    : mainItems.appendChild(mainItems.firstElementChild);
+}
+
+function translateResignWrapper(direction) {
+  console.log('first');
+  const selectedBtn = direction === 1 ? 'prev' : 'next';
+  resignItems.style.transitionDuration = '300ms';
+  if (direction === -1) {
+    resignItems.style.transform = `translateX(-1060px)`;
+  } else {
+    resignItems.style.transform = `translateX(1060px)`;
+  }
+  resignItems.ontransitionend = () => reorganizeResignWrapper(selectedBtn);
+}
+function reorganizeResignWrapper(selectedBtn) {
+  resignItems.removeAttribute('style');
+  selectedBtn === 'prev'
+    ? resignItems.insertBefore(
+        resignItems.lastElementChild,
+        resignItems.firstElementChild
+      )
+    : resignItems.appendChild(resignItems.firstElementChild);
+}
+
+function reorganizeMainItems(selectedBtn) {
+  mainItems.removeAttribute('style');
+  selectedBtn === 'prev'
+    ? mainItems.insertBefore(
+        mainItems.lastElementChild,
+        mainItems.firstElementChild
+      )
+    : mainItems.appendChild(mainItems.firstElementChild);
+}
 
 function translateContainer(direction) {
   const selectedBtn = direction === 1 ? 'prev' : 'next';
@@ -54,3 +105,16 @@ function reorganizeEl(selectedBtn) {
       )
     : container.appendChild(container.firstElementChild);
 }
+(function addEvent() {
+  let information = container.children[3].firstElementChild;
+  information.style.opacity = 1;
+  information.style.visibility = 'visible';
+  reorganizeMainItems('prev');
+
+  prevBtn.addEventListener('click', translateContainer.bind(this, 1));
+  nextBtn.addEventListener('click', translateContainer.bind(this, -1));
+  mainPrev.addEventListener('click', translateMainWrapper.bind(this, 1));
+  mainNext.addEventListener('click', translateMainWrapper.bind(this, -1));
+  resignPrev.addEventListener('click', translateResignWrapper.bind(this, 1));
+  resignNext.addEventListener('click', translateResignWrapper.bind(this, -1));
+})();

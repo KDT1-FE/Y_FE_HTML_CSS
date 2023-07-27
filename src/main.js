c8c15d3ab5d7308bbe301e21a1f60ce078f4a9c0
JSH99 <tjgus9966@naver.com>
2023-07-28 00:10:10 +0900

    [Feat] slide show 무한 자동 슬라이드 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,23 +1,37 @@
 document.addEventListener("DOMContentLoaded", function () {
     const carouselItems = document.querySelectorAll(".carousel-item");
     const navigationButtons = document.querySelectorAll(".navigation-button");
+    let currentIndex = 0;
+    let slideInterval;
 
     function showSlide(index) {
         carouselItems.forEach(item => item.style.display = "none");
         carouselItems[index].style.display = "block";
     }
 
-    showSlide(0);
-    navigationButtons[0].classList.add('active-navigation-button'); 
+    function goToNextSlide() {
+        currentIndex = (currentIndex + 1) % carouselItems.length;
+        showSlide(currentIndex);
+        updateActiveButton(currentIndex);
+    }
 
-    navigationButtons.forEach(button => {
+    function updateActiveButton(index) {
+        navigationButtons.forEach(btn => btn.classList.remove('active-navigation-button'));
+        navigationButtons[index].classList.add('active-navigation-button');
+    }
+
+    showSlide(currentIndex);
+    updateActiveButton(currentIndex);
+
+    navigationButtons.forEach((button, index) => {
         button.addEventListener("click", function () {
-            const index = parseInt(button.dataset.index);
             showSlide(index);
-
-            navigationButtons.forEach(btn => btn.classList.remove('active-navigation-button'));
-
-            button.classList.add('active-navigation-button');
+            currentIndex = index;
+            updateActiveButton(currentIndex);
+            clearInterval(slideInterval);
+            slideInterval = setInterval(goToNextSlide, 4000);
         });
     });
+
+    slideInterval = setInterval(goToNextSlide, 4000);
 });

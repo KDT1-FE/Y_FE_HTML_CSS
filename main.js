32d564dbcc29d6b492705e257dc893c302755828
gaori <monooi@naver.com>
2023-07-27 16:52:51 +0900

    Feat: Add button click animation to photos section

--- main.js
+++ main.js
@@ -20,3 +20,35 @@ sliderItems.forEach((item, itemIndex) => {
     });
   });
 });
+
+const photos = document.querySelectorAll(".airplane-photo");
+const leftBtn = document.querySelector(".left-btn");
+const rightBtn = document.querySelector(".right-btn");
+
+let currentImageIndex = 3;
+leftBtn.addEventListener("click", () => {
+  if (currentImageIndex !== 0) currentImageIndex--;
+
+  photos.forEach((photo, index) => {
+    if (index === currentImageIndex) {
+      photo.classList.add("show");
+    } else {
+      photo.classList.remove("show");
+    }
+  });
+});
+
+rightBtn.addEventListener("click", () => {
+  if (currentImageIndex !== photos.length - 1) currentImageIndex++;
+  photos.forEach((photo, index) => {
+    if (index === currentImageIndex) {
+      photo.classList.add("show");
+    } else {
+      photo.classList.remove("show");
+    }
+  });
+});
+
+// 왼쪽 버튼을 클릭하면
+// [0, 1 ,2 ,3 ,4 ,5]의 사진 중
+// 현재 사진의 이전 사진의 opacity를 1로 바꾼다

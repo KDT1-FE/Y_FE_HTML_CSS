adc51d8ad004f8c4d93064ed25d824f50ddf97a4
YongYong21 <imkinggosu@gmail.com>
2023-07-27 17:47:11 +0900

    Feat: Add changeDateFunc

--- asset/js/section.js
+++ asset/js/section.js
@@ -1,6 +1,19 @@
+// 스크롤 탑
 const stt = document.getElementsByClassName('stt')[0];
 
 stt.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
     
-  });
+    }
+);
+
+// 날짜
+const currentDate = new Date();
+const month = currentDate.getMonth() + 1; 
+const day = currentDate.getDate();
+
+const changeDate = document.getElementsByClassName('calendar')[0];
+changeDate.style.backgroundImage = `url(https://www.kakaocorp.com/page/calendar/light/ico_date${day}.gif)`;
+changeDate.innerHTML = changeDate.innerHTML.replace('7월 25일', `${month}월 ${day}일`);
+
+

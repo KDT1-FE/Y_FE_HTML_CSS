4838b37d33ecfd7a6a853f7ac066f02e32813573
flamozzi <pjs99047@gmail.com>
2023-07-27 12:27:47 +0900

    Feat: Update Youtube video section

---
+++ js/youtube.js
@@ -0,0 +1,23 @@
+// Youtube Iframe API를 비동기로 로드
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubePlayerAPIReady() {
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: '29-eutQFaKI', // 재생할 유튜브 영상 ID
+    playerVars: {
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: '29-eutQFaKI', // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      // 영상이 준비되었을 때,
+      onReady: function (event) {
+        event.target.mute(); // 음소거!
+      },
+    },
+  });
+}

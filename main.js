2952541a5fc896b63b08c69dd8a8b24d1aabec7e
gaori <monooi@naver.com>
2023-07-27 17:31:10 +0900

    Feat: Add social icons

--- main.js
+++ main.js
@@ -26,10 +26,12 @@ sliderItems.forEach((item, itemIndex) => {
 const photos = document.querySelectorAll(".airplane-photo");
 const leftBtn = document.querySelector(".left-btn");
 const rightBtn = document.querySelector(".right-btn");
+const photoIndex = document.getElementById("photo-index");
 
-let currentImageIndex = 3;
+let currentImageIndex = 0;
 leftBtn.addEventListener("click", () => {
   if (currentImageIndex !== 0) currentImageIndex--;
+  photoIndex.innerText = `${currentImageIndex + 1}/6`;
 
   photos.forEach((photo, index) => {
     if (index === currentImageIndex) {
@@ -42,6 +44,7 @@ leftBtn.addEventListener("click", () => {
 
 rightBtn.addEventListener("click", () => {
   if (currentImageIndex !== photos.length - 1) currentImageIndex++;
+  photoIndex.innerText = `${currentImageIndex + 1}/6`;
   photos.forEach((photo, index) => {
     if (index === currentImageIndex) {
       photo.classList.add("show");

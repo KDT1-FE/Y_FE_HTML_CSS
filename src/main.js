bbd03ff75fe87a0ae485504ae1c821e5e0e4f69e
89882 <pgh123166@naver.com>
2023-07-27 21:39:24 +0900

    refactor: js 중복된 코드 제거 후 함수 재사용

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ const container = document.querySelector('.items');
 const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
 const wrapper = document.querySelector('.wrapper');
-const mainWrapper = document.querySelector('.main-wrapper');
 const mainItems = document.querySelector('.main-items');
 const mainPrev = document.querySelector('.main-prev');
 const mainNext = document.querySelector('.main-next');
@@ -12,6 +11,7 @@ const resignNext = document.querySelector('.sub-next');
 const lastItems = document.querySelector('.last-items');
 const lastPrev = document.querySelector('.last-prev');
 const lastNext = document.querySelector('.last-next');
+
 let loopInterval = setInterval(() => {
   translateContainer(-1); // 다음 슬라이드를 보여주는 함수
 }, 3000);
@@ -26,69 +26,27 @@ wrapper.addEventListener('mouseover', () => {
 // 자동으로 슬라이드를 움직이게 하는 함수
 // 이 기능으로 인해 js를 사용
 
-function translateMainWrapper(direction) {
-  const selectedBtn = direction === 1 ? 'prev' : 'next';
-  mainItems.style.transitionDuration = '300ms';
-  if (direction === -1) {
-    mainItems.style.transform = `translateX(-1060px)`;
-  } else {
-    mainItems.style.transform = `translateX(1060px)`;
-  }
-  mainItems.ontransitionend = () => reorganizeMainItems(selectedBtn);
-}
-
-function reorganizeMainItems(selectedBtn) {
-  mainItems.removeAttribute('style');
-  selectedBtn === 'prev'
-    ? mainItems.insertBefore(
-        mainItems.lastElementChild,
-        mainItems.firstElementChild
-      )
-    : mainItems.appendChild(mainItems.firstElementChild);
-}
-
-function translateLastWrapper(direction) {
-  const selectedBtn = direction === 1 ? 'prev' : 'next';
-  lastItems.style.transitionDuration = '300ms';
-  if (direction === -1) {
-    lastItems.style.transform = `translateX(-1060px)`;
-  } else {
-    lastItems.style.transform = `translateX(1060px)`;
-  }
-  lastItems.ontransitionend = () => reorganizeLastItems(selectedBtn);
-}
-
-function reorganizeLastItems(selectedBtn) {
-  lastItems.removeAttribute('style');
-  selectedBtn === 'prev'
-    ? lastItems.insertBefore(
-        lastItems.lastElementChild,
-        lastItems.firstElementChild
-      )
-    : lastItems.appendChild(lastItems.firstElementChild);
-}
-
-function translateResignWrapper(direction) {
+//carousel translate 함수
+function translateCarouselWrapper(direction, items) {
   console.log('first');
   const selectedBtn = direction === 1 ? 'prev' : 'next';
-  resignItems.style.transitionDuration = '300ms';
+  items.style.transitionDuration = '300ms';
   if (direction === -1) {
-    resignItems.style.transform = `translateX(-1060px)`;
+    items.style.transform = `translateX(-1060px)`;
   } else {
-    resignItems.style.transform = `translateX(1060px)`;
+    items.style.transform = `translateX(1060px)`;
   }
-  resignItems.ontransitionend = () => reorganizeResignWrapper(selectedBtn);
+  items.ontransitionend = () => reorganizeCarouselWrapper(selectedBtn, items);
 }
-function reorganizeResignWrapper(selectedBtn) {
-  resignItems.removeAttribute('style');
+// item 이동 후 item 재배치 함수
+function reorganizeCarouselWrapper(selectedBtn, items) {
+  items.removeAttribute('style');
   selectedBtn === 'prev'
-    ? resignItems.insertBefore(
-        resignItems.lastElementChild,
-        resignItems.firstElementChild
-      )
-    : resignItems.appendChild(resignItems.firstElementChild);
+    ? items.insertBefore(items.lastElementChild, items.firstElementChild)
+    : items.appendChild(items.firstElementChild);
 }
 
+// 메인 슬라이드 translate 함수
 function translateContainer(direction) {
   const selectedBtn = direction === 1 ? 'prev' : 'next';
   container.style.transitionDuration = '500ms';
@@ -104,6 +62,7 @@ function translateContainer(direction) {
   container.ontransitionend = () => reorganizeEl(selectedBtn);
 }
 
+// main 술라이드 아이템 이동 후 재배치 함수
 function reorganizeEl(selectedBtn) {
   container.removeAttribute('style');
   let information =
@@ -120,17 +79,36 @@ function reorganizeEl(selectedBtn) {
     : container.appendChild(container.firstElementChild);
 }
 (function addEvent() {
+  // 시작을 item 1이 아닌 item 3번쨰로 조정
   let information = container.children[3].firstElementChild;
+  // 처음 화면에 보여지는 슬라이드 아이템은 information이 보여지게
   information.style.opacity = 1;
   information.style.visibility = 'visible';
-  reorganizeMainItems('prev');
 
   prevBtn.addEventListener('click', translateContainer.bind(this, 1));
   nextBtn.addEventListener('click', translateContainer.bind(this, -1));
-  mainPrev.addEventListener('click', translateMainWrapper.bind(this, 1));
-  mainNext.addEventListener('click', translateMainWrapper.bind(this, -1));
-  resignPrev.addEventListener('click', translateResignWrapper.bind(this, 1));
-  resignNext.addEventListener('click', translateResignWrapper.bind(this, -1));
-  lastPrev.addEventListener('click', translateLastWrapper.bind(this, 1));
-  lastNext.addEventListener('click', translateLastWrapper.bind(this, -1));
+  mainPrev.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, 1, mainItems)
+  );
+  mainNext.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, -1, mainItems)
+  );
+  resignPrev.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, 1, resignItems)
+  );
+  resignNext.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, -1, resignItems)
+  );
+  lastPrev.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, 1, lastItems)
+  );
+  lastNext.addEventListener(
+    'click',
+    translateCarouselWrapper.bind(this, -1, lastItems)
+  );
 })();

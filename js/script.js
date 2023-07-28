fb6e6ffe85413f6b97a4a9b2237bea0d0b9e1830
humnyenye <jisong0292@gmail.com>
2023-07-28 12:34:35 +0900

    Feat:headr, eventbanner section 작성

---
+++ js/script.js
@@ -0,0 +1,36 @@
+new Swiper('.eventBanner__swiperBox .swiper',{
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 24,
+  centeredSlides: true,
+  autoplay:{
+    delay: 5000
+  },
+  // pagination: {
+  //   el: '.promotion .swiper-pagination',
+  //   clickable: true
+  // },
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next'
+  }
+})
+
+new Swiper('.shopping__swiperBox .swiper',{
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 24,
+  centeredSlides: true,
+  autoplay:{
+    delay: 5000
+  },
+  // pagination: {
+  //   el: '.promotion .swiper-pagination',
+  //   clickable: true
+  // },
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next'
+  }
+})
+

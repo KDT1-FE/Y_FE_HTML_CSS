3a901a677cb6784861feb2db621c63b12ce5a494
humnyenye <jisong0292@gmail.com>
2023-07-28 22:57:48 +0900

    Feat: 과제 작업

---
+++ README.md
@@ -0,0 +1,28 @@
+# HTML, CSS, JS 클론 코딩 과제
+> 인터파크 메인 페이지 클론 코딩
+
+<img src="https://img.shields.io/badge/html5-#E34F26?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/css3-#1572B6?style=for-the-badge&logo=css3&logoColor=white">
+<img src="https://img.shields.io/badge/javascript-#F7DF1E?style=for-the-badge&logo=javascript&logoColor=white">
+
+## 설치 방법
+
+클론한 사이트:
+
+```
+https://www.interpark.com/
+```
+
+데모 링크:
+
+```
+
+```
+
+## 구현 사항
+
+- 스크롤 했을 때 요소가 가려지는 헤더
+- 스크롤 했을 때 searchWrap이 사라지고 navMini를 보이게 함
+- hover 하면 submenu가 펼쳐지는 nav
+- swiper 라이브러리를 이용한 다양한 형태의 swiper 구현
+- 클릭하면 페이지의 top으로 이동시켜주는 버튼
--- js/script.js
+++ js/script.js
@@ -1,36 +1,95 @@
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  const headerTop = document.querySelector('.header__top');
+  const navMini = document.querySelector('.nav__mini');
+  const searchWrap = document.querySelector('.search__wrap');
+
+  if (window.scrollY > headerTop.clientHeight) {
+    header.classList.add('fixed');
+    navMini.classList.remove('hidden');
+    searchWrap.classList.add('hidden');
+  } else {
+    header.classList.remove('fixed');
+    navMini.classList.add('hidden');
+    searchWrap.classList.remove('hidden');
+  }
+});
+
 new Swiper('.eventBanner__swiperBox .swiper',{
   loop: true,
   slidesPerView: 2,
   spaceBetween: 24,
-  centeredSlides: true,
   autoplay:{
     delay: 5000
   },
-  // pagination: {
-  //   el: '.promotion .swiper-pagination',
-  //   clickable: true
-  // },
   navigation:{
     prevEl:'.swiper-prev',
     nextEl:'.swiper-next'
   }
 })
 
-new Swiper('.shopping__swiperBox .swiper',{
-  loop: true,
-  slidesPerView: 2,
+const shoppingSwiper = new Swiper('.shopping__swiperBox .shopping__swiper .swiper',{
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 26,
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  }
+});
+const tourSwiper = new Swiper('.tour__swiperBox .tour__swiper .swiper',{
+  slidesPerView: 3,
+  slidesPerGroup: 3,
   spaceBetween: 24,
-  centeredSlides: true,
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  }
+})
+const ticketSwiper = new Swiper('.ticket__swiperBox .ticket__swiper .swiper',{
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 26,
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  }
+})
+const liveSwiper = new Swiper('.live__swiperBox .live__swiper .swiper',{
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 26,
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  }
+})
+const bookSwiper = new Swiper('.book__swiperBox .book__swiper .swiper',{
+  slidesPerView: 5,
+  slidesPerGroup: 5,
+  spaceBetween: 27.5,
+  navigation:{
+    prevEl:'.swiper-prev',
+    nextEl:'.swiper-next',
+  }
+})
+const eventSwiper = new Swiper('.event__swiperBox .event__swiper .swiper',{
+  loop:true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 26,
   autoplay:{
     delay: 5000
   },
-  // pagination: {
-  //   el: '.promotion .swiper-pagination',
-  //   clickable: true
-  // },
   navigation:{
     prevEl:'.swiper-prev',
-    nextEl:'.swiper-next'
+    nextEl:'.swiper-next',
   }
 })
 
+const toTopEl = document.querySelector('.btn__toTop .ico__arrow--up');
+toTopEl.addEventListener('click',function(){
+  gsap.to(window, 0.7, {
+    scrollTo: 0
+  });
+})

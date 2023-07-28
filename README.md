f48751fa7a96eca300c499b86da806f4040bed15
MinSeob <icdmiracle927@gmail.com>
2023-07-28 23:27:11 +0900

    Dep : Add Deploy version

--- README.md
+++ README.md
@@ -1,27 +1,69 @@
-# 💻 HTML / CSS 과제
+<img src="https://www.interpark.com/_next/static/media/ci.4836710e.svg" width="100%" height="50%" style="background:white" />
+<br/>
+<br/>
+<br/>
 
-## 📌 개요
-- **HTML/CSS** 사용을 익히고 클론 코딩을 진행한다.
-- **시맨틱 마크업**을 활용한다. 
-- 레거시 코드 활용보단 **최신의 CSS를 활용**한다.
-  
-<br><br>
+# ✈️ 인터파크 홈페이지 클론 코딩 
+ 인터파크 페이지를 클론코딩하였습니다.
+<br/>
 
-## ❤️‍🔥 사이트
+## ✅ 사이트 주소
+<a href="https://www.interpark.com/"><img src="https://img.shields.io/badge/✈️INTERPARK-참고사이트-ffffff?style=flat-square&Color=white" width="200px"/></a>
+<a href="https://internparkkimminseob.netlify.app/"><img src="https://img.shields.io/badge/✈️INTERPARK-클론코딩사이트-000000?style=flat-square&Color=black" width="230px"/></a>
 
-클론한 사이트 : https://vibe.naver.com/today <br>
-과제 사이트 : https://64c3c129234a4874663d21ca--dainty-bubblegum-b3232c.netlify.app/
+## ✅ 프로젝트 소개
+- 인터파크 홈페이지를 클론코딩하였습니다. html과 css와 야간의 js를 활용하여 홈페이지를 제작해보았습니다.
+
+## ✅ 프로젝트간 수행 목표
+### 1. HTML
+- 최대한 원 사이트와 비슷하게 제작을 진행하기
+- Semantic Tag들을 활용해서 html을 구성하기
+- 사용자 경험이 좋은 홈페이지를 만들 수 있는 UI 구성 방법을 체득하기
+
+### 2. CSS
+- Flex 기능을 통한 정렬 기능을 숙지하기
+- Relative와 abolute를 통한 버튼과 모달창 같은 기능을 구현하기
+- 원 페이지에서 어떠한 구성을 통해 가독성이 높은 페이지를 제작하였는지 알아내기
+
+## ✅ 구현 화면
+![interpark_header](/assets/image/md/sample.png)<br/>
+
+![interpark_main](/assets/image/md/sample2.png)<br/>
+
+![interpark_footer](/assets/image/md/sample3.png)
+
+<br/>
 
 
+## ✅ 주요기능 및 사용한 것
 
-<br>  
+### 1. 스크롤 반응 고정 네비게이션바 구현
+![interpark_header](/assets/image/md/g_header.gif)
+<br/>
 
-## 📝 배운 점
-- 시맨틱 마크업을 이용하여 view 구조를 잡는 법과 , position 을 사용하지 않고 flex 와 grid 를 이용하여 코드를 작성하는 경험이 공부가 많이 되었다.
+### 2. Swiper를 통한 가로방향 슬라이더 구현
+![interpark_main](/assets/image/md/g_main.gif)
+<br/>
 
-## 🚀 트러블 슈팅
-- slide 작업을 하면서 기존에 만들었던 구조가 깨지는 현상이 발생하였다.
+### 3. Hovering을 통한 모달창 구현
+![interpark_main](/assets/image/md/g_footer.gif)
 
-## 😆 해결 방안
-- min-width 를 사용하여 요소의 최소 넓이를 지정해 구조가 깨지는 현상을 막아주었다.
-<br><br>  
+## ✅ 배운점
+### 1. HTML 구성하기
+- 처음 클론코딩을 진행해보면서 UI를 사용할 때 최대한 간결하게 자세한 정보보다는 사진과 그래픽 위주로 구현하여 유저가 한눈에 페이지를 볼 수 있게 해야 한다는 것을 느끼게 되었습니다
+
+### 2. Swiper 구성하기
+- Swiper 기능을 통해서 다양한 웹사이트에서 사용되는 슬라이더 기능을 구현해볼 수 있었습니다. 이를통해 앞으로 판매관련 웹사이트를 만들 때 어떠한 구성으로 웹페이지의 슬라이더를 구현할 수 있는 지를 알게 될 수 있었습니다.
+
+### 3. CSS 구성하기
+- CSS를 직접 제작해보면서 tailwind같은 css npm library들이 얼마나 편한 유저에게 제공하는 지를 알 수 있게 되었습니다. 또한 직접 하나하나 구현해보면서 flex를 비롯한 정렬 개념과 relative abolute를 통한 블록배치 기술을 터득할 수 있었습니다.
+
+## ✅ 아쉬운점
+### 1. 반응형 웹사이트 만들기
+- CSS를 통한 반응형 웹사이트를 만드는데있어서 어려움이 있었습니다. 유저들이 모바일을 통한 기기를 많이 사용하기 때문에 수업을 통해서 반응형 웹사이트를 만들 수 있도록 노력할 것 입니다.
+
+### 2. 클린코드
+- html과 css 모두 반복되는 코드들이 상당했습니다. 특히 css의 경우 중복되는 기능들에서 약간의 차이로 인해 다시 css코드를 작성해야하는 어려움이 있었습니다. 그러다보니 코드가 계속해서 dirty해졌습니다. 따라서 학습을 통해 중복된 코드를 최대한 지향하는 클린코드를 만들 수 있도록 노력할 것입니다.
+
+### 3. Align-items를 통한 정렬
+- 각 section을 정렬하면서 align-items를 사용하였지만 제대로 구현되지 않아서 relative와 abolute를 통한 구현을 위주로 하였습니다. 따라서 절대적으로 계산에 의한 디자인이 만들어졌습니다. 이러한 문제를 해결하기위해서 앞으로는 미리 element들에 대한 구상을 진행하고 웹 페이지를 제작해야겠다는 것을 알게되었습니다.
---
+++ js/script.js
@@ -0,0 +1,139 @@
+var swiper = new Swiper('.mySwiper', {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn',
+        nextEl: '.right-btn',
+    },
+});
+
+var swiper2 = new Swiper('.mySwiper2', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-two',
+        nextEl: '.right-btn-two',
+    },
+});
+
+var swiper3 = new Swiper('.mySwiper3', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-three',
+        nextEl: '.right-btn-three',
+    },
+});
+
+var swiper4 = new Swiper('.mySwiper4', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-four',
+        nextEl: '.right-btn-four',
+    },
+});
+
+var swiper5 = new Swiper('.mySwiper5', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+
+    fade : true,
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-five',
+        nextEl: '.right-btn-five',
+    },
+});
+
+var swiper6 = new Swiper('.mySwiper6', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    slidesPerGroup: 5,
+
+    fade : true,
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-six',
+        nextEl: '.right-btn-six',
+    },
+});
+
+var swiper7 = new Swiper('.mySwiper7', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 4,
+
+    fade : true,
+    loop: true,
+    loofFillGroupWithBlank: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        prevEl: '.left-btn-seven',
+        nextEl: '.right-btn-seven',
+    },
+});
+
+window.addEventListener("scroll", function() {
+    var header = document.querySelector(".fixed-header");
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  
+    if (scrollTop > 100) { // 특정 위치(여기서는 100px) 아래로 스크롤했을 때
+      header.style.top = "0"; // 헤더를 보이도록 설정
+    } else {
+      header.style.top = "-100px"; // 헤더를 숨기도록 설정
+    }
+  });

11e16cdf9db9bf302eacd0c680da27b7d7bb72d8
SKY-PEY <lovr8537@gmail.com>
2023-07-28 12:02:04 +0900

    before remove figcaption of INFO

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
-
 // SIMPLE PARALLAX
 
 // 설문조사
-const suvey = document.getElementsByClassName('survey-thumbnail');
+const suvey = document.getElementsByClassName("survey-thumbnail");
 new simpleParallax(suvey, {
-  delay: .5,
-  orientation: 'down',
+  delay: 0.5,
+  orientation: "down",
   scale: 1.3,
   overflow: true,
   // customContainer: '.container',
   // customWrapper: '.wrapper'
 });
 
-
 // 기부와 나눔
-const share = document.getElementsByClassName('share-thumbnail');
+const share = document.getElementsByClassName("share-thumbnail");
 new simpleParallax(share, {
-  delay: .5,
-  orientation: 'down',
+  delay: 0.5,
+  orientation: "down",
   scale: 1.3,
   overflow: true,
 });
-
 
 // 매장 찾기
-const shop = document.getElementsByClassName('shop-thumbnail');
+const shop = document.getElementsByClassName("shop-thumbnail");
 new simpleParallax(shop, {
-  delay: .5,
-  orientation: 'down',
+  delay: 0.5,
+  orientation: "down",
   scale: 1.3,
   overflow: true,
 });
-
-
 
 // SWIPER
 
-
 // 러쉬 소개
 
-const swiper__info =document.querySelector('.swiper__info');
+const swiper__info = document.querySelector(".swiper__info");
 new Swiper(swiper__info, {
   slidesPerView: 4,
   centeredSlides: true,
@@ -47,38 +41,37 @@ new Swiper(swiper__info, {
   grabCursor: true,
 });
 
-
 // 카테고리
-const swiper__category = document.querySelector('.swiper__category');
+const swiper__category = document.querySelector(".swiper__category");
 new Swiper(swiper__category, {
   slidesPerView: 1,
-  //centeredSlides: true,
   spaceBetween: 50,
-  grabCursor: true,
   autoplay: {
     delay: 5000,
   },
   loop: true,
-  crossfade : true,
-  loopAdditionalSlides : 1,
-  pagination: { 
-  el: ".swiper-pagination", 
-  clickable: true,
-  type : "bullet",
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    type: "bullet",
   },
 });
-
 
 // 카테고리__원형이미지
 const swiper__circle = document.querySelector(".swiper__circle");
 new Swiper(swiper__circle, {
-  slidesPerView: 4,
- // centeredSlides: true,
+  slidesPerView: auto,
+  // centeredSlides: true,
   spaceBetween: 5,
   grabCursor: true,
+
   autoplay: {
-    delay:1050,
+    delay: 1000,
+    disableOnInteraction: false,
   },
   loop: true,
-  loopAdditionalSlides : 1,
 });

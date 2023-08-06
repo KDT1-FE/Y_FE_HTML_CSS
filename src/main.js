f0f5422a725cb363a5c453df2c274b8d236574b0
89882 <pgh123166@naver.com>
2023-08-06 15:31:35 +0900

    refactor: slider 및 banner 이동 너비 변수 선언

--- src/main.js
+++ src/main.js
@@ -1,30 +1,35 @@
 // top banner
-const topBannerItems = document.querySelector('.items');
-const topPrev = document.querySelector('.top-prev');
-const topNext = document.querySelector('.top-next');
-const topBannerWrapper = document.querySelector('.wrapper');
+const topBannerItems = document.querySelector(".items");
+const topPrev = document.querySelector(".top-prev");
+const topNext = document.querySelector(".top-next");
+const topBannerWrapper = document.querySelector(".wrapper");
 //main slider
-const mainItems = document.querySelector('.main-items');
-const mainPrev = document.querySelector('.main-prev');
-const mainNext = document.querySelector('.main-next');
+const mainItems = document.querySelector(".main-items");
+const mainPrev = document.querySelector(".main-prev");
+const mainNext = document.querySelector(".main-next");
 //sub slider
-const subItems = document.querySelector('.sub-items');
-const subPrev = document.querySelector('.sub-prev');
-const subNext = document.querySelector('.sub-next');
+const subItems = document.querySelector(".sub-items");
+const subPrev = document.querySelector(".sub-prev");
+const subNext = document.querySelector(".sub-next");
 //last slider
-const lastItems = document.querySelector('.last-items');
-const lastPrev = document.querySelector('.last-prev');
-const lastNext = document.querySelector('.last-next');
+const lastItems = document.querySelector(".last-items");
+const lastPrev = document.querySelector(".last-prev");
+const lastNext = document.querySelector(".last-next");
+
+// translateX 이동 너비 변수 선언
+const sliderWidth = 1060;
+const topBannerPrev = 560;
+const topBannerNext = -1620;
 
 let loopInterval = setInterval(() => {
   translateContainer(-1); // 다음 슬라이드를 보여주는 함수
 }, 3000);
-topBannerWrapper.addEventListener('mouseout', () => {
+topBannerWrapper.addEventListener("mouseout", () => {
   loopInterval = setInterval(() => {
     translateContainer(-1); // 다음 슬라이드를 보여주는 함수
   }, 3000);
 });
-topBannerWrapper.addEventListener('mouseover', () => {
+topBannerWrapper.addEventListener("mouseover", () => {
   clearInterval(loopInterval);
 });
 // tob banner을 자동으로 슬라이드를 움직이게 하는 함수
@@ -32,34 +37,33 @@ topBannerWrapper.addEventListener('mouseover', () => {
 
 //carousel translate 함수
 function translateCarouselWrapper(direction, items) {
-  const selectedBtn = direction === 1 ? 'prev' : 'next';
-  items.style.transitionDuration = '300ms';
+  const selectedBtn = direction === 1 ? "prev" : "next";
   if (direction === -1) {
-    items.style.transform = `translateX(-1060px)`;
+    items.style.transform = `translateX(-${sliderWidth}px)`;
   } else {
-    items.style.transform = `translateX(1060px)`;
+    items.style.transform = `translateX(${sliderWidth}px)`;
   }
   items.ontransitionend = () => reorganizeCarouselWrapper(selectedBtn, items);
 }
 // item 이동 후 item 재배치 함수
 function reorganizeCarouselWrapper(selectedBtn, items) {
-  items.removeAttribute('style');
-  selectedBtn === 'prev'
+  items.removeAttribute("style");
+  selectedBtn === "prev"
     ? items.insertBefore(items.lastElementChild, items.firstElementChild)
     : items.appendChild(items.firstElementChild);
 }
 
 // 메인 슬라이드 translate 함수
 function translateContainer(direction) {
-  const selectedBtn = direction === 1 ? 'prev' : 'next';
-  topBannerItems.style.transitionDuration = '500ms';
+  const selectedBtn = direction === 1 ? "prev" : "next";
+  topBannerItems.style.transitionDuration = "500ms";
   let information = topBannerItems.children[3].firstElementChild;
-  information.style.visibility = 'hidden';
+  information.style.visibility = "hidden";
   information.style.opacity = 0;
   if (direction === -1) {
-    topBannerItems.style.transform = `translateX(-1620px)`;
+    topBannerItems.style.transform = `translateX(${topBannerNext}px)`;
   } else {
-    topBannerItems.style.transform = `translateX(560px)`;
+    topBannerItems.style.transform = `translateX(${topBannerPrev}px)`;
   }
   //5초동안 gap + 이미지 크기 + 이미지 절반 크기인 1620px만큼 움직임
   topBannerItems.ontransitionend = () => reorganizeEl(selectedBtn);
@@ -67,14 +71,14 @@ function translateContainer(direction) {
 
 // main 술라이드 아이템 이동 후 재배치 함수
 function reorganizeEl(selectedBtn) {
-  topBannerItems.removeAttribute('style');
+  topBannerItems.removeAttribute("style");
   let information =
-    selectedBtn === 'prev'
+    selectedBtn === "prev"
       ? topBannerItems.children[2].firstElementChild
       : topBannerItems.children[4].firstElementChild;
   information.style.opacity = 1;
-  information.style.visibility = 'visible';
-  selectedBtn === 'prev'
+  information.style.visibility = "visible";
+  selectedBtn === "prev"
     ? topBannerItems.insertBefore(
         topBannerItems.lastElementChild,
         topBannerItems.firstElementChild
@@ -86,32 +90,32 @@ function reorganizeEl(selectedBtn) {
   let information = topBannerItems.children[3].firstElementChild;
   // 처음 화면에 보여지는 슬라이드 아이템은 information이 보여지게
   information.style.opacity = 1;
-  information.style.visibility = 'visible';
+  information.style.visibility = "visible";
 
-  topPrev.addEventListener('click', translateContainer.bind(this, 1));
-  topNext.addEventListener('click', translateContainer.bind(this, -1));
+  topPrev.addEventListener("click", translateContainer.bind(this, 1));
+  topNext.addEventListener("click", translateContainer.bind(this, -1));
   mainPrev.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, 1, mainItems)
   );
   mainNext.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, -1, mainItems)
   );
   subPrev.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, 1, subItems)
   );
   subNext.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, -1, subItems)
   );
   lastPrev.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, 1, lastItems)
   );
   lastNext.addEventListener(
-    'click',
+    "click",
     translateCarouselWrapper.bind(this, -1, lastItems)
   );
 })();

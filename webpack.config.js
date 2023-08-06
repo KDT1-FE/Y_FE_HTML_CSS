d3843b24d19f45dd5f3c7bbae467999e5a63416a
gaori <monooi@naver.com>
2023-08-07 00:48:50 +0900

    Chore: Create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};

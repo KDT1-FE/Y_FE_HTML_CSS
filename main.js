0c08cebfa4524eeee0db235d2e06c787a2ff860e
mysdpy30s <mysdpy30s@gmail.com>
2023-08-07 16:03:51 +0900

    camelcase로 작성된 클래스명 수정

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("my-slides");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -15,35 +15,33 @@ function showSlides() {
   setTimeout(showSlides, 7000); // Change image every 5 seconds
 }
 
-
 // Get the elements needed
-const logoImage = document.getElementById('logoImage');
-const innerArea = document.querySelector('nav');
+const logoImage = document.getElementById("logoImage");
+const innerArea = document.querySelector("nav");
 
 // Add event listener to handle hover
-innerArea.addEventListener('mouseenter', () => {
+innerArea.addEventListener("mouseenter", () => {
   // Change the image source to logo_h.png when hovering
   logoImage.src = "/images/logo_h.png";
 });
 
-innerArea.addEventListener('mouseleave', () => {
+innerArea.addEventListener("mouseleave", () => {
   // Change the image source back to logo.png when not hovering
   logoImage.src = "/images/logo.png";
 });
 
-
 // Get the elements needed
-const sidesImages = document.querySelectorAll('.sides img');
+const sidesImages = document.querySelectorAll(".sides img");
 
 // Function to change the image on hover
 function changeImageOnHover(event) {
   const image = event.target;
-  const altImageSrc = image.getAttribute('data-hover-src');
+  const altImageSrc = image.getAttribute("data-hover-src");
 
   if (altImageSrc) {
     // Store the original source in a custom attribute to switch back later
-    if (!image.getAttribute('data-original-src')) {
-      image.setAttribute('data-original-src', image.src);
+    if (!image.getAttribute("data-original-src")) {
+      image.setAttribute("data-original-src", image.src);
     }
 
     image.src = altImageSrc;
@@ -53,7 +51,7 @@ function changeImageOnHover(event) {
 // Function to switch back to the original image when not hovered
 function switchBackToOriginal(event) {
   const image = event.target;
-  const originalSrc = image.getAttribute('data-original-src');
+  const originalSrc = image.getAttribute("data-original-src");
 
   if (originalSrc) {
     image.src = originalSrc;
@@ -63,19 +61,17 @@ function switchBackToOriginal(event) {
 // Add event listeners to handle hover effect on all images in the .sides list
 sidesImages.forEach((image) => {
   // Extract the original source from the 'src' attribute and store it as 'data-original-src'
-  image.setAttribute('data-original-src', image.src);
-  image.addEventListener('mouseenter', changeImageOnHover);
-  image.addEventListener('mouseleave', switchBackToOriginal);
+  image.setAttribute("data-original-src", image.src);
+  image.addEventListener("mouseenter", changeImageOnHover);
+  image.addEventListener("mouseleave", switchBackToOriginal);
 });
 
-
-
 // Get the elements needed
-const menuRightTop = document.querySelectorAll('.menu_right_top a');
-const menuRightBot = document.querySelectorAll('.menu_right_bottom a');
-const hoveredArea = document.querySelector('.menu_center');
+const menuRightTop = document.querySelectorAll(".menu_right_top a");
+const menuRightBot = document.querySelectorAll(".menu_right_bottom a");
+const hoveredArea = document.querySelector(".menu_center");
 // Add event listener to handle hover
-hoveredArea.addEventListener('mouseenter', () => {
+hoveredArea.addEventListener("mouseenter", () => {
   menuRightTop.forEach((anchor) => {
     anchor.style.color = "#242424";
   });
@@ -84,7 +80,7 @@ hoveredArea.addEventListener('mouseenter', () => {
   });
 });
 
-hoveredArea.addEventListener('mouseleave', () => {
+hoveredArea.addEventListener("mouseleave", () => {
   // Change the font color of all anchor elements back to #F0DEC8 when not hovering
   menuRightTop.forEach((anchor) => {
     anchor.style.color = "#fff";
@@ -97,8 +93,8 @@ hoveredArea.addEventListener('mouseleave', () => {
 /**
  * 슬라이드 요소 관리
  */
-new Swiper('.notice .swiper-container', {
-  direction: 'vertical', // 수직 슬라이드
+new Swiper(".notice .swiper-container", {
+  direction: "vertical", // 수직 슬라이드
   autoplay: true, // 자동 재생 여부
-  loop: true // 반복 재생 여부
-})
+  loop: true, // 반복 재생 여부
+});

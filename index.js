7dfb2d2b6a0223ce1c7282dd146171c441da4c8b
jisu Seo <jseo9732@gmail.com>
2023-08-07 12:00:21 +0900

    Refactor: addEventListener 코드 개선

--- index.js
+++ index.js
@@ -17,17 +17,11 @@ window.addEventListener("scroll", function () {
 
 // 질문 아코디언 기능
 
-let question1 = document.getElementById("question1");
-let question2 = document.getElementById("question2");
-let question3 = document.getElementById("question3");
-let question4 = document.getElementById("question4");
-let question5 = document.getElementById("question5");
+const questions = document.querySelectorAll(".accordion-container");
 
-question1.addEventListener("click", () => click(question1));
-question2.addEventListener("click", () => click(question2));
-question3.addEventListener("click", () => click(question3));
-question4.addEventListener("click", () => click(question4));
-question5.addEventListener("click", () => click(question5));
+for (const question of questions) {
+  question.addEventListener("click", () => click(question));
+}
 
 function click(question) {
   question.classList.toggle("open");

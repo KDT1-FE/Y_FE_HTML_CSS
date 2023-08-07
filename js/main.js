ed2f3260b6af8358fe787778a03390ac20c7633c
suehub <dldustn2040@gmail.com>
2023-08-07 22:24:23 +0900

    Feat: Add Aside Menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
+const scrollToTop = document.querySelector('.scrolltop__button');
 const SECONDS_TO_MS = 1000;
+
+ScrollToTheTop();
 
 const mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
@@ -14,3 +17,23 @@ const mySwiper = new Swiper('.swiper-container', {
     },
     effect: 'fade'
 });
+
+function ScrollToTheTop () {
+    // 스크롤에 따라 버튼 display
+    window.addEventListener('scroll', () => {
+        if (document.querySelector('html').scrollTop > 750) {
+        scrollToTop.style.display = "block";
+        } else {
+        scrollToTop.style.display = "none";
+        }
+    });
+
+    // 클릭 시 상단으로 이동
+    scrollToTop.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+}

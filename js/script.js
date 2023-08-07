133c0c7e90951411741cf6ac676b1fbcee2f054e
humnyenye <jisong0292@gmail.com>
2023-08-07 23:05:11 +0900

    Fix: 리팩토링

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-window.addEventListener('scroll', function() {
+const throttle=(callback, limit = 100) => {
+  let waiting = false
+  return function() {
+      if(!waiting) {
+          callback.apply(this, arguments)
+          waiting = true
+          setTimeout(() => {
+              waiting = false
+          }, limit)
+      }
+  }
+}
+
+window.addEventListener('scroll', throttle(()=>{
   const header = document.querySelector('header');
   const headerTop = document.querySelector('.header__top');
   const navMini = document.querySelector('.nav__mini');
   const searchWrap = document.querySelector('.search__wrap');
+  console.log('fixed')
 
   if (window.scrollY > headerTop.clientHeight) {
     header.classList.add('fixed');
@@ -13,7 +27,7 @@ window.addEventListener('scroll', function() {
     navMini.classList.add('hidden');
     searchWrap.classList.remove('hidden');
   }
-});
+},100));
 
 new Swiper('.eventBanner__swiperBox .swiper',{
   loop: true,
@@ -23,8 +37,8 @@ new Swiper('.eventBanner__swiperBox .swiper',{
     delay: 5000
   },
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next'
+    prevEl:'.swiper-prev__eventBanner',
+    nextEl:'.swiper-next__eventBanner'
   }
 })
 
@@ -33,8 +47,8 @@ const shoppingSwiper = new Swiper('.shopping__swiperBox .shopping__swiper .swipe
   slidesPerGroup: 4,
   spaceBetween: 26,
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__shopping',
+    nextEl:'.swiper-next__shopping',
   }
 });
 const tourSwiper = new Swiper('.tour__swiperBox .tour__swiper .swiper',{
@@ -42,8 +56,8 @@ const tourSwiper = new Swiper('.tour__swiperBox .tour__swiper .swiper',{
   slidesPerGroup: 3,
   spaceBetween: 24,
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__tour',
+    nextEl:'.swiper-next__tour',
   }
 })
 const ticketSwiper = new Swiper('.ticket__swiperBox .ticket__swiper .swiper',{
@@ -51,8 +65,8 @@ const ticketSwiper = new Swiper('.ticket__swiperBox .ticket__swiper .swiper',{
   slidesPerGroup: 4,
   spaceBetween: 26,
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__ticket',
+    nextEl:'.swiper-next__ticket',
   }
 })
 const liveSwiper = new Swiper('.live__swiperBox .live__swiper .swiper',{
@@ -60,8 +74,8 @@ const liveSwiper = new Swiper('.live__swiperBox .live__swiper .swiper',{
   slidesPerGroup: 4,
   spaceBetween: 26,
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__live',
+    nextEl:'.swiper-next__live',
   }
 })
 const bookSwiper = new Swiper('.book__swiperBox .book__swiper .swiper',{
@@ -69,8 +83,8 @@ const bookSwiper = new Swiper('.book__swiperBox .book__swiper .swiper',{
   slidesPerGroup: 5,
   spaceBetween: 27.5,
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__book',
+    nextEl:'.swiper-next__book',
   }
 })
 const eventSwiper = new Swiper('.event__swiperBox .event__swiper .swiper',{
@@ -82,8 +96,8 @@ const eventSwiper = new Swiper('.event__swiperBox .event__swiper .swiper',{
     delay: 5000
   },
   navigation:{
-    prevEl:'.swiper-prev',
-    nextEl:'.swiper-next',
+    prevEl:'.swiper-prev__event',
+    nextEl:'.swiper-next__event',
   }
 })
 

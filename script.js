6dcca1f95b71064fed5f5074cb3164f8dc5070a0
applevalley <skatkd7571@naver.com>
2023-08-07 17:00:31 +0900

    Refactor: Design nav

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ head_banner.addEventListener("click", function () {
   let parentNode = head_banner.parentNode.parentNode;
   parentNode.classList.add("hidden");
 });
+
+const header_mobile = document.getElementsByClassName("header__mobile_menu")[0];
+
+function mobile_nav_menu() {
+  let childElement = header_mobile.lastElementChild;
+  childElement.style.display = "flex";
+  header_mobile.classList.add("mobile_nav");
+}

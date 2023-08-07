d0c750628e45a8a4925c4c0af7130983c4d579be
suehub <dldustn2040@gmail.com>
2023-08-07 19:28:04 +0900

    Refactor: html, css, js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+const SECONDS_TO_MS = 1000;
+
 const mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
     autoplay: {
-        delay: 5500
+        delay: 5.5 * SECONDS_TO_MS
     },
-    speed: 1000,
+    speed: 1 * SECONDS_TO_MS,
     slidesPerView: 1,
     navigation: {
         nextEl: '.swiper-button-next',

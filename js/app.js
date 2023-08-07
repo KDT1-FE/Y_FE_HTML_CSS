3a831dabf94f14acd2ac2d47b29e1e96d591f270
sunpaaaa <sunpaaaa@gmail.com>
2023-08-07 21:42:15 +0900

    Remove unnecessary variable from app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 const toggle = document.querySelector('header .toggle');
 const navbar = document.querySelector('.navbar-box');
-let hide = true;
 
 toggle.addEventListener('click', () => {
   navbar.classList.toggle('hide');
-  hide = !hide;
 });

bf32884cd020a3b07853d511422fccb09b484fd6
SOL-MI <ovo10203@gmail.com>
2023-08-07 11:35:17 +0900

    Refactor:마우스, 스크롤 이벤트

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 //헤더 호버 관련 선언
-const hovermenus = document.getElementsByClassName("has-submenu")
-let arr = []
-for(i=0;i<hovermenus.length;i++){
-  arr.push(hovermenus[i])
-}
+const hovermenus = [...document.getElementsByClassName("has-submenu")]
+
 
 //헤더 호버&스크롤 관련 선언
 const headerBg = document.querySelector("header")
@@ -15,7 +12,7 @@ const headerBorderColor = document.querySelector(".header-inner-wrapper")
 let isMouseOver = false;
 
 
-arr.forEach(item => {
+hovermenus.forEach(item => {
   //마우스 올렸을 때 헤더 변화 
   item.addEventListener('mouseover',function(){
     isMouseOver = true;
@@ -43,14 +40,13 @@ window.addEventListener('scroll',_.throttle(function(){
       gsap.to(headerTextColor, .1, {color:'black'})
       gsap.to(headerRightTextColor, .1, {color:'black'})
       gsap.to(headerBorderColor, .1, {borderBottomColor:'black'})
-    }else if((window.scrollY<200)&&isMouseOver){
+    }else if(isMouseOver){
       gsap.to(whiteLogo, 0, {display:'none'})
       gsap.to(blackLogo, 0, {display:'block'})
       gsap.to(headerBg, .1, {backgroundColor:'white'})
       gsap.to(headerTextColor, .1, {color:'black'})
       gsap.to(headerRightTextColor, .1, {color:'black'})
       gsap.to(headerBorderColor, .1, {borderBottomColor:'black'})
-      console.log('itsworking')
     }else{
       gsap.to(whiteLogo, 0, {display:'block'})
       gsap.to(blackLogo, 0, {display:'none'})
@@ -59,7 +55,7 @@ window.addEventListener('scroll',_.throttle(function(){
       gsap.to(headerRightTextColor, .1, {color:'white'})
       gsap.to(headerBorderColor, .1, {borderBottomColor:'transparent'})
     }
-},200))
+},200),{ passive: true })
 
 //스크롤 내리면 섹션이 떠오르며 나타나는 효과
 window.addEventListener('scroll', _.throttle(function() {
@@ -68,9 +64,8 @@ window.addEventListener('scroll', _.throttle(function() {
     { selector: ".appear2", offset: 1000},
     { selector: ".appear3", offset: 1800},
     { selector: ".appear4", offset: 2700},
-    { selector: ".appear5", offset: 3400},
+    { selector: ".appear5", offset: 3400}
   ];
-  console.log()
 
   elements.forEach(({selector, offset}) => {
     const element = document.querySelector(selector);
@@ -86,16 +81,15 @@ window.addEventListener('scroll', _.throttle(function() {
   });
 }, 200));
 
-function getType(data){
-  return Object.prototype.toString.call(data).slice(8,-1)
-}
+// function getType(data){
+//   return Object.prototype.toString.call(data).slice(8,-1)
+// }
 
+const SECONDS_TO_MS = 1000;
 //슬라이더
 new Swiper('.main-introduction .swiper-container', {
-  autoplay: { // 자동 재생 여부
-  delay: 4000 // 5초마다 슬라이드 바뀜
-  },
-  loop: true, // 반복 재생 여부
+  autoplay: { delay: 4*SECONDS_TO_MS },
+  loop: true,
   slidesPerView: 1, // 한 번에 보여줄 슬라이드 개수
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   pagination: { // 페이지 번호 사용 여부
@@ -105,9 +99,7 @@ new Swiper('.main-introduction .swiper-container', {
 })
 new Swiper('.products-container .swiper-container', {
   direction: 'horizontal',
-  autoplay:{
-    delay:3000
-  },
+  autoplay:{ delay:3*SECONDS_TO_MS },
   loop:false,
   slidesPerView:3,
   navigation: { // 슬라이드 이전/다음 버튼 사용 여부
@@ -117,25 +109,21 @@ new Swiper('.products-container .swiper-container', {
 })
 new Swiper('.select-shop-container .swiper-container', {
   direction: 'horizontal',
-  autoplay:{
-    delay:3000
-  },
+  autoplay:{delay:3*SECONDS_TO_MS},
   loop:false,
   slidesPerView:3,
-  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-    prevEl: '.select-shop-container .swiper-prev', // 이전 버튼 선택자
-    nextEl: '.select-shop-container .swiper-next' // 다음 버튼 선택자
+  navigation: { 
+    prevEl: '.select-shop-container .swiper-prev', 
+    nextEl: '.select-shop-container .swiper-next'
   }
 })
 new Swiper('.business-solution-slider .swiper-container', {
   direction: 'horizontal',
-  autoplay:{
-    delay:3000
-  },
+  autoplay:{delay:3*SECONDS_TO_MS},
   loop:true,
   slidesPerView: 1,
-  pagination: { // 페이지 번호 사용 여부
-    el: '.business-solution-slider .swiper-pagination', // 페이지 번호 요소 선택자
-    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  pagination: {
+    el: '.business-solution-slider .swiper-pagination', 
+    clickable: true
   }
 })
